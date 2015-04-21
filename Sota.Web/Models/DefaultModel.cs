b4777c074f4785c6ba56b772e1fee26cc4c2ef04
Adigun Palmer <adigunpalmer@hotmail.com>
2015-04-21 06:57:10 -0400

    Working MVC Components

---
+++ Sota.Web/Models/DefaultModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Sota.Web.Models
+{
+    public class DefaultModel
+    {
+        public String Name { get; set; }
+    }
+}
--- Sota.Web/Startup.cs
+++ Sota.Web/Startup.cs
@@ -10,10 +10,22 @@ namespace Sota.Web
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add MVC services to the services container.
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Default", action = "Index" });
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
         }
     }
 }
